849cb64d9447cc45f1f134d99a0bf0faa7965c6c
sunardi1511 <sunardi1511@gmail.com>
2022-08-22 11:52:13 +0800

    commit pertama

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
+const fs = require('fs')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const { Media } = require('../models');
+
+router.get('/', async(req,res) => {
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
+
+  const mapMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`
+    return m;
+  })
+
+  return res.json({
+    status: 'suksess',
+    data: mapMedia
+  })
+})
+
+router.post('/', (req, res) => {
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: 'error', message: 'invalid base64' });
+  }
+
+  base64Img.img(image, './public/images', Date.now(), async (err, filepath) => {
+    if (err) {
+      return res.status(400).json({ status: 'error', message: err.message })
+    }
+
+    const filename = filepath.split('/').pop();
+
+    const media = await Media.create({ image: `images/${filename}` });
+
+    return res.json({
+      status: 'sukses',
+      data: {
+        id: media.id,
+        image: `${req.get('host')}/images/${filename}`
+      }
+    })
+  })
+})
+
+router.delete('/:id', async(req, res) => {
+  const id = req.params.id;
+  
+  const media = await Media.findByPk (id);
+  if(!media) {
+    return res.send(404).json({status: 'error', message: 'media not found'}); 
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if(err) {
+      return res.status(400).json({
+        status: 'error', message: err.message
+      })
+    }
+  })
+
+  await media.destroy()
+  
+  return res.json({
+    status: 'suksess',
+    message: ' image berhasil di hapus'
+  })
+})
 
 module.exports = router;
